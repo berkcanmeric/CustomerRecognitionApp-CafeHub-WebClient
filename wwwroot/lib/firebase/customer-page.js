5a216987717406d1bf08ef2534d89a11dfa3969f
Berkcan Meric <berkcanmeric99@hotmail.com>
2023-05-11 00:25:40 +0300

    Customer comment section added.

--- wwwroot/lib/firebase/customer-page.js
+++ wwwroot/lib/firebase/customer-page.js
@@ -15,14 +15,33 @@ import {
     startAfter,
 } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js";
 
+
 // Initialize Firestore
 const db = getFirestore(app);
 
-// Get a reference to the Firestore collection
-const usersRef = collection(db, "User");
+const cafeId = "Acv7hYavbdraEyHibjK4";
 
-// Get a reference to the table element
-const table = document.getElementById("customer-table");
+// Get a reference to the Firestore document
+const cafeDocRef = doc(db, "Cafe", cafeId);
+
+const activeUsersRef = collection(cafeDocRef, "ActiveUserList");
+// Get a reference to the Firestore collection "Rating"
+const ratingsRef = collection(db, "Rating");
+
+// Get all documents in ActiveUserList 
+const activeUserDocs = await getDocs(activeUsersRef);
+
+// Get an array of all userids from those documents
+const userIds = activeUserDocs.docs.map(doc => doc.data().userId);
+
+const usersQueryRef = query(
+    collection(db, "User"),
+    where("id", "in", userIds)
+);
+const ratingsQueryRef = query(ratingsRef, orderBy("ratingDate"));
+
+const usersQuerySnapshot = await getDocs(usersQueryRef);
+const ratingsQuerySnapshot = await getDocs(ratingsQueryRef);
 
 async function initTable() {
     // Call the getData function to load the initial table data
@@ -32,15 +51,20 @@ async function initTable() {
 
 async function getData() {
     try {
-        let queryRef = query(usersRef, orderBy("name"));
-        const querySnapshot = await getDocs(queryRef);
-
         // Clear the table before adding new data (except for the headers)
         $('#customer-table').DataTable().clear();
+        $('#rating-table').DataTable().clear();
 
-        // Loop through the query snapshot and append the customer data to the table
-        querySnapshot.forEach((docc) => {
-            const data = docc.data();
+        // Get all user documents with matching IDs
+        const usersQuerySnapshot = await getDocs(usersQueryRef);
+        const users = {};
+        // Loop through the user documents and add a row to the table for each one
+        usersQuerySnapshot.forEach((doc) => {
+            const data = doc.data();
+            users[data.id] = {
+                name: `${data.name} ${data.surname}`,
+                photoUrl: data.photoUrl
+            };
             $('#customer-table').DataTable().row.add([
                 data.name,
                 data.surname,
@@ -49,9 +73,33 @@ async function getData() {
                 `<img src="${data.photoUrl}" alt="User Photo" class="user-photo" data-name="${data.name} ${data.surname}"/>`
             ]).draw().node();
         });
+        console.log(users);
 
+        // Loop through the rating documents and add a row to the table for each one
+        const ratingsQuerySnapshot = await getDocs(ratingsQueryRef);
+        ratingsQuerySnapshot.forEach((doc) => {
+            const data = doc.data();
+            const date = data.ratingDate.toDate().toLocaleDateString();
+            const user = users[data.userId];
+            let userPhoto;
+            if (user) {
+                userPhoto = `<img src="${user.photoUrl}" alt="${user.name}" class="user-photo" data-name="${user.name}"/>`;
+            } else {
+                userPhoto = '<img src="/lib/img/user.png" alt="User Photo" class="user-photo" data-name="Unknown User"/>';
+            }
+            const userName = user ? user.name : "Unknown User";
+            const nameAndPhoto = `<span class="user-name p-2">${userName}</span>  ${userPhoto}`;
+            $('#rating-table').DataTable().row.add([
+                nameAndPhoto,
+                data.comment,
+                date,
+                [...Array(data.score)].map(() => '<i class="fas fa-star"></i> ').join(''),
+            ]).draw().node();
+        });
+        
         // Redraw the table to update the pagination
         $('#customer-table').DataTable().draw();
+        $('#rating-table').DataTable().draw();
     } catch (err) {
         console.error("Error getting documents:", err);
     }
