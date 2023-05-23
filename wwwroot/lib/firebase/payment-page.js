074c92fbd09af3a0e5bda9326d5fb6dafa131539
Berkcan Meric <berkcanmeric99@hotmail.com>
2023-05-23 22:29:51 +0300

    Payment section added.

--- wwwroot/lib/firebase/payment-page.js
+++ wwwroot/lib/firebase/payment-page.js
@@ -6,7 +6,9 @@ import {
     getDocs,
     doc,
     query,
-    where
+    where,
+    addDoc,
+    updateDoc,
 } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js";
 
 // Initialize Firestore
@@ -56,7 +58,8 @@ async function getUserData() {
                     data.name,
                     data.price,
                     data.category,
-                    `<img src="${data.imageUrl}" alt="Product Image" class="product-image"/>`
+                    `<img src="${data.imageUrl}" alt="Product Image" class="product-image"/>`,
+                    data.id, // Add the 'data-id' attribute as a hidden column
                 ]).draw().node();
             });
             // Loop through the categories in the query snapshot
@@ -71,43 +74,16 @@ async function getUserData() {
                 // Add a click event listener to the category button
                 button.click(function () {
                     // Filter the "Category" column to show only the clicked category
-                    console.log($(this).text());
                     $('#product-table').DataTable().columns(3).search($(this).text()).draw();
                 });
                 // Add the button to the categories container element
                 $('#categories').append(button);
             });
 
-            function showToast(message, type = 'error') {
-                const toast = document.createElement('div');
-                toast.className = `toast bg-${type}`;
-                toast.setAttribute('role', 'alert');
-                toast.setAttribute('aria-live', 'assertive');
-                toast.setAttribute('aria-atomic', 'true');
-                toast.innerHTML = `
-                <div class="toast-header">
-                  <strong class="me-auto">${type === 'success' ? 'Başarılı' : 'Hata'}</strong>
-                  <button type="button" class="btn-close ms-2 mb-1" data-bs-dismiss="toast" aria-label="Close"></button>
-                </div>
-                <div class="toast-body">
-                  ${message}
-                </div>
-                `;
-                const container = document.createElement('div');
-                container.className = 'toast-container';
-                container.appendChild(toast);
-                document.body.appendChild(container);
-                const toastInstance = new bootstrap.Toast(toast);
-                toastInstance.show();
-            }
-
-
             if (!usersQuerySnapshot.empty) {
                 // User found with the matching ID
                 const userDoc = usersQuerySnapshot.docs[0];
                 const userData = userDoc.data();
-                console.log("User found:", userData);
-
                 // Update the offcanvas fields with user information
                 const userPhotoElement = document.getElementById("userPhoto");
                 const userNameElement = document.getElementById("userName");
@@ -136,6 +112,181 @@ async function getUserData() {
     }
 }
 
+$('#checkout-button').on('click', async function () {
+    debugger;
+    // Get the selected product rows
+    const table = $('#product-table').DataTable(); // Initialize the table with DataTables
+
+    // Get the selected product rows
+    const selectedRows = table.rows('.selected');
+    if (selectedRows.count() === 0) {
+        showToast('No products selected for checkout.', 'error');
+        return;
+    }
+
+    const selectedProducts = [];
+    const selectedRowsData = selectedRows.data(); // Get the data for the selected rows
+    for (let i = 0; i < selectedRowsData.length; i++) {
+        const product = selectedRowsData[i];
+        if (typeof product !== 'undefined') { // Check if the product variable is defined
+            selectedProducts.push({
+                id: product[5], // Use the value of the 'data-id' attribute as the product ID
+                name: product[1], // Assuming the first column contains the product name
+                price: product[2], // Assuming the second column contains the product price
+                category: product[3], // Assuming the third column contains the product category
+            });
+        }
+    }
+    
+    // Create the order object
+    const order = {
+        userId: userId,
+        cafeId: cafeId,
+        products: selectedProducts,
+        cost: calculateTotalCost(selectedProducts), // Calculate the total cost
+        isRated:false,
+        SpotifyPermissions: {
+            totalTracks: 1,
+            until: new Date(Date.now() + 3600000),
+        },
+        time: new Date(), // Current time
+    };
+    
+    try {
+        // Add the order to the Firestore collection
+        await addDoc(collection(db, 'Order'), order); // Use the top-level "Order" collection
+        
+        // Clear the selected products and update the off-canvas view
+        const selectedRowsNodes = selectedRows.nodes(); // Get the DOM nodes of the selected rows
+        $(selectedRowsNodes).removeClass('selected'); // Remove the 'selected' class from the selected rows
+        var checkbox=$(selectedRowsNodes).find('input[type="checkbox"]'); // Remove the checkmarks from the selected rows
+        checkbox.prop('checked', !checkbox.prop('checked'));      
+        updateSelectedProductsCount();
+        await addCouponToUser(userId, cafeId);
+        showToast('Order placed successfully.', 'success');
+    } catch (error) {
+        showToast('Error placing order. Please try again later.', 'error');
+        console.error('Error placing order:', error);
+    }
+});
+
+function showToast(message, type = 'error') {
+    const toast = document.createElement('div');
+    toast.className = `toast bg-${type}`;
+    toast.setAttribute('role', 'alert');
+    toast.setAttribute('aria-live', 'assertive');
+    toast.setAttribute('aria-atomic', 'true');
+    toast.innerHTML = `
+                <div class="toast-header">
+                  <strong class="me-auto">${type === 'success' ? 'Başarılı' : 'Hata'}</strong>
+                  <button type="button" class="btn-close ms-2 mb-1" data-bs-dismiss="toast" aria-label="Close"></button>
+                </div>
+                <div class="toast-body">
+                  ${message}
+                </div>
+                `;
+    const container = document.createElement('div');
+    container.className = 'toast-container';
+    container.appendChild(toast);
+    document.body.appendChild(container);
+    const toastInstance = new bootstrap.Toast(toast);
+    toastInstance.show();
+}
+function calculateTotalCost(products) {
+    let totalCost = 0;
+    products.forEach((product) => {
+        totalCost += parseFloat(product.price);
+    });
+    return totalCost.toFixed(2);
+}
+function updateSelectedProductsCount() {
+    const selectedRowCount =  $('#product-table').DataTable().rows('.selected').data().length;
+    $('#selected-products-count').text(selectedRowCount + ' ürün seçildi');
+
+    const selectedProducts =  $('#product-table').DataTable().rows('.selected').data().toArray(); // Get the selected products as an array
+    updateSelectedProducts(selectedProducts);
+}
+function updateSelectedProducts(selectedProducts) {
+    const selectedProductsContainer = document.getElementById("selected-products-container");
+    selectedProductsContainer.innerHTML = ""; // Clear the container before adding new products
+
+    selectedProducts.forEach((product) => {
+        const productElement = document.createElement("div");
+        productElement.classList.add("selected-product");
+
+        const productImageHTML = product[4];  // Product image HTML is at index 4
+        productElement.innerHTML = productImageHTML;
+
+        const productDetails = document.createElement("div");
+        productDetails.classList.add("product-details");
+
+        const productName = document.createElement("span");
+        productName.textContent = product[1]; // Product name is at index 1
+        productName.classList.add("product-name");
+        productDetails.appendChild(productName);
+
+        const productPrice = document.createElement("span");
+        productPrice.textContent = " " + product[2] + " TL"; // Product price is at index 2
+        productPrice.classList.add("product-price");
+        productDetails.appendChild(productPrice);
+
+        productElement.appendChild(productDetails);
+        selectedProductsContainer.appendChild(productElement);
+    });
+}
+async function addCouponToUser(userId, cafeId) {
+    // Generate a random 8 character alphanumeric coupon code
+    const couponCode = generateCouponCode();
+
+    // Get a reference to the coupon collection
+    const couponCollection = collection(db, "Coupon");
+
+    // Check if a document with the same userId and cafeId exists
+    const querySnapshot = await getDocs(
+        query(couponCollection, where("userId", "==", userId), where("cafeId", "==", cafeId))
+    );
+
+    if (querySnapshot.empty) {
+        // No existing document found, create a new one
+        await addDoc(couponCollection, {
+            userId: userId,
+            cafeId: cafeId,
+            couponCode: "",
+            couponCount: 1
+        });
+    } else {
+        // Increment the coupon count of the existing document
+        querySnapshot.forEach((doc) => {
+            const currentCouponCount = doc.data().couponCount;
+
+            if (currentCouponCount < 5) {
+                updateDoc(doc.ref, {
+                    couponCount: currentCouponCount + 1,
+                    couponCode: couponCode
+                });
+            } else {
+                updateDoc(doc.ref, {
+                    couponCount: currentCouponCount + 1,
+                    couponCode: ""
+                });
+            }
+        });
+    }
+}
+
+
+
+function generateCouponCode() {
+    let coupon = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    const charactersLength = characters.length;
+
+    for (let i = 0; i < 8; i++) {
+        coupon += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return coupon;
+}
 // Call the getUserData function to fetch and display user information
 getUserData();
 
